bb8abe1288ee1d381735886ac318eaf15f1fe76f
Rasmilakc <139522774+Rasmilakc@users.noreply.github.com>
2023-07-18 17:15:46 -0500

    java.js

---
+++ java.js
@@ -0,0 +1,12 @@
+print("hello world")
+#git status-to track new files and change over file
+#git init-initialize repo with git
+#git add .-all the files move to staging
+#git add java.js-one file from working to staging
+#git commit -m"firstfeature"(configure email addressand username)
+
+#go to github and create repository
+#add and make it public
+git branch -M main-creating a main branch
+#git remote add origin https://github.com/Rasmilakc/gitPractice1.gitconnect a local repo to git remote repo 
+#git push -u origin main push the code and check repo
